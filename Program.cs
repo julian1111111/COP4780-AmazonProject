ebfae368fd13a4dad9b12eb9985bea5dda47caca
julian1111111 <145719754+julian1111111@users.noreply.github.com>
2025-01-28 14:50:25 -0500

    Some setup

--- Program.cs
+++ Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+using System;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Amazon!");
+
+            Console.WriteLine("I: Inventory management");
+            Console.WriteLine("S: Shopping cart management");
+
+            Console.WriteLine("C: Create new inventory item");
+            Console.WriteLine("R: Read all inventory items");
+            Console.WriteLine("U: Update an inventory item");
+            Console.WriteLine("D: Delete an inventory item");
+
+            string? input = Console.ReadLine();
+        }
+    }
+}
