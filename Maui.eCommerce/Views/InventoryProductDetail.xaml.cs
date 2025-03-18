5c27289bd77f4c03ffcf11ae3e843bdd85ca89a6
julian1111111 <145719754+julian1111111@users.noreply.github.com>
2025-03-18 15:04:13 -0400

    Begins implementation of interactive inventory product addition page

--- Maui.eCommerce/Views/InventoryProductDetail.xaml.cs
+++ Maui.eCommerce/Views/InventoryProductDetail.xaml.cs
@@ -1,16 +1,27 @@
+using AmazonProject.Models;
+using Library.eCommerce.Services;
 using Maui.eCommerce.ViewModels;
 
 namespace Maui.eCommerce.Views;
 
 public partial class InventoryProductDetail : ContentPage
 {
-	public InventoryProductDetail()
+    private ProductServiceProxy _svc = ProductServiceProxy.Current;
+
+    public InventoryProductDetail()
 	{
 		InitializeComponent();
         BindingContext = new ProductViewModel();
 	}
     private void CancelClicked(object sender, EventArgs e)
     {
+        Shell.Current.GoToAsync("//InventoryManagement");
+    }
+
+    private void OkClicked(object sender, EventArgs e)
+    {
+        var name = (BindingContext as ProductViewModel).Name;
+        _svc.AddOrUpdateInventory(new Product { Name = name });
         Shell.Current.GoToAsync("//InventoryManagement");
     }
 }
